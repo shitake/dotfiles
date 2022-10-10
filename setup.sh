0e6bb432ee6b7228c5bf58a1892db8c13dd81aa5
shitake <p230584300813995212@gmail.com>
2022-10-10 14:46:58 +0900

    add Starship conf

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-DOT_FILES=( .zsh .zshrc .zshenv .gdbinit .gitconfig .gitignore .vimrc .gvimrc .tmux.conf .dir_colors )
+DOT_FILES=( .zsh .zshrc .zshenv .gdbinit .gitconfig .gitignore .vimrc .gvimrc .tmux.conf .dir_colors .config/starship.toml )
 
 for file in ${DOT_FILES[@]}
 do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
-
